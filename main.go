7b311063720bd4cdb412d194fd7eaa90596769c7
Breno Santos <brenosantos145@gmail.com>
2025-01-30 12:39:26 -0300

    feat: read post files and save

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+type Post struct {
+	Title    string
+	Content  template.HTML
+	Filename string
+}
+
+var (
+	postsDir     = "posts"
+	outputDir    = "public"
+	templatesDir = "templates"
+
+	postTemplate  = template.Must(template.ParseFiles(filepath.Join(templatesDir, "post.html")))
+	indexTemplate = template.Must(template.ParseFiles(filepath.Join(templatesDir, "index.html")))
+)
+
+func main() {
+	var posts []Post
+	files, err := os.ReadDir(postsDir)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, f := range files {
+		if f.IsDir() || !strings.HasSuffix(f.Name(), ".md") {
+			continue
+		}
+
+		post := Post{
+			Title:    strings.TrimSuffix(f.Name(), ".md"),
+			Filename: filepath.Join(postsDir, f.Name()),
+		}
+
+		posts = append(posts, post)
+	}
+
+	for _, post := range posts {
+		fmt.Println(post.Filename)
+	}
+}
---
+++ posts/test copy.md
@@ -0,0 +1,3 @@
+# Test
+
+texto de teste 2
